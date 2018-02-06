399285190ea376ab01d796ca1ab48ff0d4ff1af9
Mike Egger <m.egger@e-mx.at>
2018-02-06 12:55:23 +0100

    removed gdx dependencies

--- at/gousoogames/dungeons/gui/output/controller/InputManagerFactory.java
+++ at/gousoogames/dungeons/gui/output/controller/InputManagerFactory.java
@@ -7,7 +7,7 @@ public class InputManagerFactory {
 	private static IInputManager instance = null;
 	
 	public static IInputManager getInputManager(){
-		if (instance == null) instance = new InputManagerGUI();
+		if (instance == null) instance = new InputManagerText();
 		return instance;
 	}
 	
--- at/gousoogames/dungeons/gui/output/controller/OutputFactory.java
+++ at/gousoogames/dungeons/gui/output/controller/OutputFactory.java
@@ -7,7 +7,7 @@ public class OutputFactory {
 	private static IOutput instance = null;
 	
 	public static IOutput getOutput(){
-		if (instance == null) instance = new OutputGUI();
+		if (instance == null) instance = new OutputText();
 		return instance;
 	}
 	
